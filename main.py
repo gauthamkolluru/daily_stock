9dab05ff82ecc6990f720eb911cdd5160d4c5125
Sai Gautham Kolluru <gautham.kolluru@gmail.com>
2023-09-24 16:24:21 -0400

    removed api key from code and added to environ variables

--- main.py
+++ main.py
@@ -22,50 +22,19 @@ def get_config(config_file):
     return config
 
 
-config = get_config(ABS_CONFIG_PATH)
-
-stock_api = config['api']['stock_api']
-news_api = config['api']['news_api']
-
-api_key = config['api']['api_key']
-
-
 def get_trending(url, api_key):
     response = requests.get(url.format(api_key))
     data = response.json()['most_actively_traded']
     return data
 
 
-data = get_trending(stock_api, api_key)
-
-
 def get_tickers(data):
     return [i['ticker'] for i in data]
 
 
-tickers = get_tickers(data=data)
-
-
-def get_ticker_news(url, api_key, tickers):
-    news = {}
-    for ticker in tickers:
-        response = requests.get(url.format(ticker, api_key))
-        news.update({ticker: response.json()})
-    return news
-
-
-news = get_ticker_news(news_api, api_key, tickers)
-
-db_name = 'daily_tick.db'
-
-cnx = sqlite3.connect(db_name)
-
-df = pd.read_sql_query("SELECT * FROM trending_ticker;", cnx)
-
-
-cnx.close()
-
-pprint(df.head())
+def get_ticker_news(url, api_key, ticker):
+    response = requests.get(url.format(ticker, api_key)).json()
+    return response
 
 
 # Todo: Write ticker data to daily_tick.db (sqlite3)
@@ -75,8 +44,6 @@ pprint(df.head())
 # Todo: get the whole volume of stock available for a ticker to calculate trade_volume/total_volume to get the percentage of trade volume >>> yf.Ticker("AAPL").info['volume']
 # Todo: schedule this to run once every 2 mins from 9:31 AM to 10:30 AM
 # Todo: Send email with the ticker and relevant news at 10:31 AM
-
-# pprint(news)
 
 
 def get_ticker_info():
@@ -89,3 +56,55 @@ def gather_info():
 
 def send_email():
     pass
+
+
+def update_ticker_name(ticker_data):
+    for ticker in ticker_data:
+        ticker_name = yf.Ticker(ticker['ticker']).info['shortName']
+        ticker.update({'ticker_name': ticker_name})
+    return ticker_data
+
+
+def convert_to_num(ticker_data):
+    columns = ['change_percentage', 'change_amount', 'volume', 'price']
+    for ticker in ticker_data:
+        for col in columns:
+            if ticker[col][-1] == '%':
+                ticker[col] = ticker[col][:-1]
+            ticker[col] = float(ticker[col])
+    return ticker_data
+
+
+def main():
+    api_key = os.environ.get('ALPHAVANTAGE_FREE_APIKEY')
+
+    config = get_config(ABS_CONFIG_PATH)
+
+    stock_api = config['alphavantage']['stock_api']
+    news_api = config['alphavantage']['news_api']
+
+    trending_ticker_data = get_trending(stock_api, api_key)
+    trending_ticker_data = update_ticker_name(trending_ticker_data)
+    trending_ticker_data = convert_to_num(trending_ticker_data)
+
+    sorted_ticker_data = sorted(
+        trending_ticker_data, key=lambda x: x['change_percentage'], reverse=True)
+
+    tickers = [i['ticker'] for i in sorted_ticker_data]
+
+    for ticker in tickers:
+        news = get_ticker_news(news_api, api_key, ticker['ticker'])
+
+    db_name = 'daily_tick.db'
+
+    cnx = sqlite3.connect(db_name)
+
+    df_db = pd.read_sql_query("SELECT * FROM trending_ticker;", cnx)
+
+    cnx.close()
+
+    pprint(df_db.head())
+
+
+if __name__ == '__main__':
+    main()
